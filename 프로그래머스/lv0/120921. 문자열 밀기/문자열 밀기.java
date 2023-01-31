a6db44355b4ae268fe366db6527ad54344ce91e2
dani820 <70650493+dani820@users.noreply.github.com>
2023-01-31 13:52:00 +0900

    Update 문자열 밀기.java

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
@@ -5,13 +5,11 @@ class Solution {
         if (!B.equals(A)) {
             for (int i = 0; i < A.length(); i++) {
                 String lastChar = A.substring(A.length() - 1);
-                System.out.println(lastChar);
+                
                 A = A.substring(0, A.length() - 1);
                 A = lastChar + A;
                 answer++;
 
-                System.out.println(A);
-                System.out.println("=====================");
                 if (B.equals(A)) {
                     return answer;
                 }
