f36355be8b6ca7461f7b157bbb937451d1ab7536
dani820 <70650493+dani820@users.noreply.github.com>
2023-01-31 14:19:58 +0900

    [level 0] Title: 문자열 밀기, Time: 0.02 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,22 +1,6 @@
 class Solution {
     public int solution(String A, String B) {
-        int answer = 0;
-        
-        if (!B.equals(A)) {
-            for (int i = 0; i < A.length(); i++) {
-                String lastChar = A.substring(A.length() - 1);
-                
-                A = A.substring(0, A.length() - 1);
-                A = lastChar + A;
-                answer++;
-
-                if (B.equals(A)) {
-                    return answer;
-                }
-            }
-            answer = -1;    
-        }
-        
-        return answer;
+        String tempB = B.repeat(3);
+        return tempB.indexOf(A);
     }
 }
