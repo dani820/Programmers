60c310112a2bd6e5fcabb1d48393f08aa3b6b96b
dani820 <70650493+dani820@users.noreply.github.com>
2023-02-10 16:05:01 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        if (Math.pow((int)Math.sqrt(n), 2) == n) {
+            answer = 1;
+        } else {
+            answer = 2;
+        }
+        return answer;
+    }
+}
