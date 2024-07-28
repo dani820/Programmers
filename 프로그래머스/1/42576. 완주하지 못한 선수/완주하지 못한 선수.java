41f1fea7de05254a9c570b2da25ff0dd4d37c41a
DaIn Park <70650493+dani820@users.noreply.github.com>
2024-07-28 11:59:32 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 84.67 ms, Memory: 96.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String key : participant) {
+            map.put(key, map.getOrDefault(key, 0) + 1);    
+        }
+        for (String key : completion) {
+            map.put(key, map.get(key) - 1);
+        }
+        for (String key : map.keySet()) {
+            if (map.get(key) > 0) answer = key;
+        }
+        
+        return answer;
+    }
+}
